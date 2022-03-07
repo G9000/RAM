bce072ec3f53888b3ecf52a712a35312f76945f2
G9000 <leo.caesar@live.com>
2022-03-07 12:06:09 +0800

    change deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,15 +5,16 @@ const main = async () => {
 
   console.log("Contract deployed to:", domainContract.address);
 
-  let txn = await domainContract.register("mars", {
+  // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of bananas lol
+  let txn = await domainContract.register("julio", {
     value: hre.ethers.utils.parseEther("0.1"),
   });
   await txn.wait();
-  console.log("Minted domain juliocaesar.mars");
+  console.log("Minted domain julio.mars");
 
-  txn = await domainContract.setRecord("julio", "https://juliocaesar.co");
+  txn = await domainContract.setRecord("julio", "Am I on mars??");
   await txn.wait();
-  console.log("Set record for juliocaesar.mars");
+  console.log("Set record for julio.mars");
 
   const address = await domainContract.getAddress("julio");
   console.log("Owner of domain julio:", address);
